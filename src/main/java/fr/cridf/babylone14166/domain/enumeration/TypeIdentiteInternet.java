cb5cecf921fd0c7eb4cadd9b8e27763decf2ba26
Mathieu Lorber <mathieu.lorber@gmail.com>
2015-12-08 19:27:55 +0100

    Updates discussion avec Solveig

--- src/main/java/fr/cridf/babylone14166/domain/enumeration/TypeIdentiteInternet.java
+++ src/main/java/fr/cridf/babylone14166/domain/enumeration/TypeIdentiteInternet.java
@@ -4,5 +4,5 @@ package fr.cridf.babylone14166.domain.enumeration;
  * The TypeIdentiteInternet enumeration.
  */
 public enum TypeIdentiteInternet {
-    Blog, Twitter, LinkedIn, Viadeo, Facebook, GooglePlus, Autre
+    SiteInternet, Blog, Twitter, LinkedIn, Viadeo, Facebook, GooglePlus, Autre
 }
