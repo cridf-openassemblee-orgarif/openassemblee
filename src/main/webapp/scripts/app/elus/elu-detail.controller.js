ae42763a961827f563b9b7e1336a380c51649922
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-01-14 16:22:16 +0100

    Fix important display bugs pour elu

--- src/main/webapp/scripts/app/elus/elu-detail.controller.js
+++ src/main/webapp/scripts/app/elus/elu-detail.controller.js
@@ -9,11 +9,12 @@ angular.module('babylone14166App')
                 $scope.dto.$promise.then(function () {
                     updateScope();
                 });
-            } else if ($scope.r.elu) {
+            } else if ($scope.dto.elu) {
                 updateScope();
             }
         });
         var updateScope = function () {
+            $scope.elu = $scope.dto.elu;
             if ($scope.dto.elu.appartenancesCommissionPermanente &&
                 $scope.dto.elu.appartenancesCommissionPermanente.length > 0) {
                 angular.forEach($scope.dto.elu.appartenancesCommissionPermanente, function (a) {
