108032a7f77690e2a743e35060938d1df1425fa7
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-09-12 16:02:19 +0200

    fix auditTrail dates bug

--- src/main/webapp/scripts/components/entities/auditTrail/auditTrail.service.js
+++ src/main/webapp/scripts/components/entities/auditTrail/auditTrail.service.js
@@ -8,21 +8,18 @@ angular.module('babylone14166App')
                 method: 'GET',
                 transformResponse: function (data) {
                     data = angular.fromJson(data);
-                    data.date = DateUtils.convertLocaleDateFromServer(data.date);
                     return data;
                 }
             },
             'update': {
                 method: 'PUT',
                 transformRequest: function (data) {
-                    data.date = DateUtils.convertLocaleDateToServer(data.date);
                     return angular.toJson(data);
                 }
             },
             'save': {
                 method: 'POST',
                 transformRequest: function (data) {
-                    data.date = DateUtils.convertLocaleDateToServer(data.date);
                     return angular.toJson(data);
                 }
             }
