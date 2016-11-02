ebeb575fb4a6594dee2fb3124936a5b283081af2
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-11-02 16:39:57 +0100

    Séance tri dans l'ordre chronologique desc

--- src/main/webapp/scripts/app/entities/seance/seance.controller.js
+++ src/main/webapp/scripts/app/entities/seance/seance.controller.js
@@ -2,11 +2,11 @@
 
 angular.module('babylone14166App')
     .controller('SeanceController', function ($scope, $state, $modal, Seance, SeanceSearch, ParseLinks) {
-      
+
         $scope.seances = [];
         $scope.page = 0;
         $scope.loadAll = function() {
-            Seance.query({page: $scope.page, size: 20}, function(result, headers) {
+            Seance.query({page: $scope.page, size: 20, sort: 'date,desc'}, function(result, headers) {
                 $scope.links = ParseLinks.parse(headers('link'));
                 $scope.seances = result;
             });
