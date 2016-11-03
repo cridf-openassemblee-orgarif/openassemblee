e29395be96105fd396c2813e36efd17f059e64f4
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-11-03 18:37:52 +0100

    filtres sur les pouvoirs d'une séance : actifs/non actifs ou par groupe politique (combinable) + petit fix affichage date+heure complète

--- src/main/webapp/scripts/app/entities/seance/seance-detail.controller.js
+++ src/main/webapp/scripts/app/entities/seance/seance-detail.controller.js
@@ -3,12 +3,54 @@
 angular.module('babylone14166App')
     .controller('SeanceDetailController', function ($scope, $rootScope, $stateParams, entity, Seance) {
         $scope.dto = entity;
+
+        $scope.computeGroupesPolitiques = function () {
+            var groupesPolitiques = {};
+            $scope.dto.pouvoirs.map(function (p) {
+                if (p.eluCedeur.groupePolitique != null && groupesPolitiques[p.eluCedeur.groupePolitique.id] == null) {
+                    groupesPolitiques[p.eluCedeur.groupePolitique.id] = p.eluCedeur.groupePolitique;
+                }
+                if (p.eluBeneficiaire.groupePolitique != null && groupesPolitiques[p.eluBeneficiaire.groupePolitique.id] == null) {
+                    groupesPolitiques[p.eluBeneficiaire.groupePolitique.id] = p.eluBeneficiaire.groupePolitique;
+                }
+            });
+            return groupesPolitiques;
+        };
+
+        $scope.computePouvoirs = function () {
+            return $scope.dto.pouvoirs
+                .filter(function (pvDto) {
+                    switch ($scope.filtre.state) {
+                        case 'tous':
+                            return true;
+                        case 'actifs':
+                            return pvDto.pouvoir.dateFin == null || pvDto.pouvoir.heureFin == null;
+                        case 'nonActifs':
+                            return pvDto.pouvoir.dateFin != null && pvDto.pouvoir.heureFin != null;
+                    }
+                    return true;
+                })
+                .filter(function(pvDto) {
+                    if ($scope.filtre.groupePolitique == 'tous') {
+                        return true;
+                    }
+                    var gpId = $scope.filtre.groupePolitique;
+                    return (pvDto.eluCedeur.groupePolitique != null && pvDto.eluCedeur.groupePolitique.id == gpId)
+                        || (pvDto.eluBeneficiaire.groupePolitique != null && pvDto.eluBeneficiaire.groupePolitique.id == gpId);
+                });
+        };
+
+        $scope.filtre = {
+            state: 'tous',
+            groupePolitique: 'tous'
+        };
+
         $scope.load = function (id) {
-            Seance.getDto({id: id}, function(result) {
+            Seance.getDto({id: id}, function (result) {
                 $scope.dto = result;
             });
         };
-        var unsubscribe = $rootScope.$on('babylone14166App:seanceUpdate', function(event, result) {
+        var unsubscribe = $rootScope.$on('babylone14166App:seanceUpdate', function (event, result) {
             $scope.seance = result;
         });
         $scope.$on('$destroy', unsubscribe);
