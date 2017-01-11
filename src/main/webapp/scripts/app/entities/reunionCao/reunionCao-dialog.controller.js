3834e4bbf408b6fe60c79d4a6cc76fbd63c96bf4
Mathieu Lorber <mathieu.lorber@gmail.com>
2017-01-11 21:04:10 +0100

    format

--- src/main/webapp/scripts/app/entities/reunionCao/reunionCao-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/reunionCao/reunionCao-dialog.controller.js
@@ -2,35 +2,35 @@
 
 angular.module('babylone14166App').controller('ReunionCaoDialogController',
     ['$scope', '$stateParams', '$modalInstance', 'entity', 'ReunionCao',
-        function($scope, $stateParams, $modalInstance, entity, ReunionCao) {
+        function ($scope, $stateParams, $modalInstance, entity, ReunionCao) {
 
-        $scope.reunionCao = entity;
-        $scope.load = function(id) {
-            ReunionCao.get({id : id}, function(result) {
-                $scope.reunionCao = result;
-            });
-        };
+            $scope.reunionCao = entity;
+            $scope.load = function (id) {
+                ReunionCao.get({id: id}, function (result) {
+                    $scope.reunionCao = result;
+                });
+            };
 
-        var onSaveSuccess = function (result) {
-            $scope.$emit('babylone14166App:reunionCaoUpdate', result);
-            $modalInstance.close(result);
-            $scope.isSaving = false;
-        };
+            var onSaveSuccess = function (result) {
+                $scope.$emit('babylone14166App:reunionCaoUpdate', result);
+                $modalInstance.close(result);
+                $scope.isSaving = false;
+            };
 
-        var onSaveError = function (result) {
-            $scope.isSaving = false;
-        };
+            var onSaveError = function (result) {
+                $scope.isSaving = false;
+            };
 
-        $scope.save = function () {
-            $scope.isSaving = true;
-            if ($scope.reunionCao.id != null) {
-                ReunionCao.update($scope.reunionCao, onSaveSuccess, onSaveError);
-            } else {
-                ReunionCao.save($scope.reunionCao, onSaveSuccess, onSaveError);
-            }
-        };
+            $scope.save = function () {
+                $scope.isSaving = true;
+                if ($scope.reunionCao.id != null) {
+                    ReunionCao.update($scope.reunionCao, onSaveSuccess, onSaveError);
+                } else {
+                    ReunionCao.save($scope.reunionCao, onSaveSuccess, onSaveError);
+                }
+            };
 
-        $scope.clear = function() {
-            $modalInstance.dismiss('cancel');
-        };
-}]);
+            $scope.clear = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        }]);
