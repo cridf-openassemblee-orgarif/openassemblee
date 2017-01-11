c816d0c9f77fd5db23b9983f90784cf38eeb6eb9
Mathieu Lorber <mathieu.lorber@gmail.com>
2017-01-11 18:52:15 +0100

    format

--- src/main/webapp/scripts/app/entities/seance/seance-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/seance/seance-dialog.controller.js
@@ -2,36 +2,36 @@
 
 angular.module('babylone14166App').controller('SeanceDialogController',
     ['$scope', '$stateParams', '$modalInstance', 'entity', 'Seance', 'PresenceElu',
-        function($scope, $stateParams, $modalInstance, entity, Seance, PresenceElu) {
+        function ($scope, $stateParams, $modalInstance, entity, Seance, PresenceElu) {
 
-        $scope.seance = entity;
-        // $scope.presenceelus = PresenceElu.query();
-        $scope.load = function(id) {
-            Seance.get({id : id}, function(result) {
-                $scope.seance = result;
-            });
-        };
+            $scope.seance = entity;
+            // $scope.presenceelus = PresenceElu.query();
+            $scope.load = function (id) {
+                Seance.get({id: id}, function (result) {
+                    $scope.seance = result;
+                });
+            };
 
-        var onSaveSuccess = function (result) {
-            $scope.$emit('babylone14166App:seanceUpdate', result);
-            $modalInstance.close(result);
-            $scope.isSaving = false;
-        };
+            var onSaveSuccess = function (result) {
+                $scope.$emit('babylone14166App:seanceUpdate', result);
+                $modalInstance.close(result);
+                $scope.isSaving = false;
+            };
 
-        var onSaveError = function (result) {
-            $scope.isSaving = false;
-        };
+            var onSaveError = function (result) {
+                $scope.isSaving = false;
+            };
 
-        $scope.save = function () {
-            $scope.isSaving = true;
-            if ($scope.seance.id != null) {
-                Seance.update($scope.seance, onSaveSuccess, onSaveError);
-            } else {
-                Seance.save($scope.seance, onSaveSuccess, onSaveError);
-            }
-        };
+            $scope.save = function () {
+                $scope.isSaving = true;
+                if ($scope.seance.id != null) {
+                    Seance.update($scope.seance, onSaveSuccess, onSaveError);
+                } else {
+                    Seance.save($scope.seance, onSaveSuccess, onSaveError);
+                }
+            };
 
-        $scope.clear = function() {
-            $modalInstance.dismiss('cancel');
-        };
-}]);
+            $scope.clear = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        }]);
