467ff62094d2ff8f63019636ef0018d3b4d1ea72
Mathieu Lorber <mathieu.lorber@gmail.com>
2017-01-12 01:13:15 +0100

    format

--- src/main/webapp/scripts/app/entities/appartenanceCommissionPermanente/appartenanceCommissionPermanente-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/appartenanceCommissionPermanente/appartenanceCommissionPermanente-dialog.controller.js
@@ -2,37 +2,37 @@
 
 angular.module('babylone14166App').controller('AppartenanceCommissionPermanenteDialogController',
     ['$scope', '$stateParams', '$modalInstance', 'entity', 'AppartenanceCommissionPermanente', 'Elu',
-        function($scope, $stateParams, $modalInstance, entity, AppartenanceCommissionPermanente, Elu) {
+        function ($scope, $stateParams, $modalInstance, entity, AppartenanceCommissionPermanente, Elu) {
 
-        $scope.appartenanceCommissionPermanente = entity;
-        $scope.elus = Elu.query();
-        $scope.load = function(id) {
-            AppartenanceCommissionPermanente.get({id : id}, function(result) {
-                $scope.appartenanceCommissionPermanente = result;
-            });
-        };
+            $scope.appartenanceCommissionPermanente = entity;
+            $scope.elus = Elu.query();
+            $scope.load = function (id) {
+                AppartenanceCommissionPermanente.get({id: id}, function (result) {
+                    $scope.appartenanceCommissionPermanente = result;
+                });
+            };
 
-        var onSaveSuccess = function (result) {
-            $scope.$emit('babylone14166App:appartenanceCommissionPermanenteUpdate', result);
-            $modalInstance.close(result);
-            $scope.isSaving = false;
-        };
+            var onSaveSuccess = function (result) {
+                $scope.$emit('babylone14166App:appartenanceCommissionPermanenteUpdate', result);
+                $modalInstance.close(result);
+                $scope.isSaving = false;
+            };
 
-        var onSaveError = function (result) {
-            $scope.isSaving = false;
-        };
+            var onSaveError = function (result) {
+                $scope.isSaving = false;
+            };
 
-        $scope.save = function () {
-            $scope.isSaving = true;
-            $scope.appartenanceCommissionPermanente.elu = {id: $stateParams.id};
-            if ($scope.appartenanceCommissionPermanente.id != null) {
-                AppartenanceCommissionPermanente.update($scope.appartenanceCommissionPermanente, onSaveSuccess, onSaveError);
-            } else {
-                AppartenanceCommissionPermanente.save($scope.appartenanceCommissionPermanente, onSaveSuccess, onSaveError);
-            }
-        };
+            $scope.save = function () {
+                $scope.isSaving = true;
+                $scope.appartenanceCommissionPermanente.elu = {id: $stateParams.id};
+                if ($scope.appartenanceCommissionPermanente.id != null) {
+                    AppartenanceCommissionPermanente.update($scope.appartenanceCommissionPermanente, onSaveSuccess, onSaveError);
+                } else {
+                    AppartenanceCommissionPermanente.save($scope.appartenanceCommissionPermanente, onSaveSuccess, onSaveError);
+                }
+            };
 
-        $scope.clear = function() {
-            $modalInstance.dismiss('cancel');
-        };
-}]);
+            $scope.clear = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        }]);
--- src/main/webapp/scripts/app/entities/fonctionCommissionThematique/fonctionCommissionThematique-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/fonctionCommissionThematique/fonctionCommissionThematique-dialog.controller.js
@@ -2,38 +2,38 @@
 
 angular.module('babylone14166App').controller('FonctionCommissionThematiqueDialogController',
     ['$scope', '$stateParams', '$modalInstance', 'entity', 'FonctionCommissionThematique', 'Elu', 'CommissionThematique',
-        function($scope, $stateParams, $modalInstance, entity, FonctionCommissionThematique, Elu, CommissionThematique) {
+        function ($scope, $stateParams, $modalInstance, entity, FonctionCommissionThematique, Elu, CommissionThematique) {
 
-        $scope.fonctionCommissionThematique = entity;
-        $scope.elus = Elu.query();
-        $scope.commissionthematiques = CommissionThematique.query();
-        $scope.load = function(id) {
-            FonctionCommissionThematique.get({id : id}, function(result) {
-                $scope.fonctionCommissionThematique = result;
-            });
-        };
+            $scope.fonctionCommissionThematique = entity;
+            $scope.elus = Elu.query();
+            $scope.commissionthematiques = CommissionThematique.query();
+            $scope.load = function (id) {
+                FonctionCommissionThematique.get({id: id}, function (result) {
+                    $scope.fonctionCommissionThematique = result;
+                });
+            };
 
-        var onSaveSuccess = function (result) {
-            $scope.$emit('babylone14166App:fonctionCommissionThematiqueUpdate', result);
-            $modalInstance.close(result);
-            $scope.isSaving = false;
-        };
+            var onSaveSuccess = function (result) {
+                $scope.$emit('babylone14166App:fonctionCommissionThematiqueUpdate', result);
+                $modalInstance.close(result);
+                $scope.isSaving = false;
+            };
 
-        var onSaveError = function (result) {
-            $scope.isSaving = false;
-        };
+            var onSaveError = function (result) {
+                $scope.isSaving = false;
+            };
 
-        $scope.save = function () {
-            $scope.isSaving = true;
-            $scope.fonctionCommissionThematique.elu = {id: $stateParams.id};
-            if ($scope.fonctionCommissionThematique.id != null) {
-                FonctionCommissionThematique.update($scope.fonctionCommissionThematique, onSaveSuccess, onSaveError);
-            } else {
-                FonctionCommissionThematique.save($scope.fonctionCommissionThematique, onSaveSuccess, onSaveError);
-            }
-        };
+            $scope.save = function () {
+                $scope.isSaving = true;
+                $scope.fonctionCommissionThematique.elu = {id: $stateParams.id};
+                if ($scope.fonctionCommissionThematique.id != null) {
+                    FonctionCommissionThematique.update($scope.fonctionCommissionThematique, onSaveSuccess, onSaveError);
+                } else {
+                    FonctionCommissionThematique.save($scope.fonctionCommissionThematique, onSaveSuccess, onSaveError);
+                }
+            };
 
-        $scope.clear = function() {
-            $modalInstance.dismiss('cancel');
-        };
-}]);
+            $scope.clear = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        }]);
