21ffe74b02453654ee1d5db3be294deabc470137
Mathieu Lorber <mathieu.lorber@gmail.com>
2018-05-24 18:14:23 +0200

    Fix signatures réunions CT / CAO + compte

--- src/main/webapp/scripts/app/entities/reunionCommissionThematique/reunionCommissionThematique-detail.controller.js
+++ src/main/webapp/scripts/app/entities/reunionCommissionThematique/reunionCommissionThematique-detail.controller.js
@@ -3,6 +3,25 @@
 angular.module('openassembleeApp')
     .controller('ReunionCommissionThematiqueDetailController', function ($scope, $rootScope, $stateParams, entity, ReunionCommissionThematique) {
         $scope.reunion = entity;
+        $scope.signaturesCount = {
+            totalMissing: 0
+        };
+
+        $scope.updateSignaturesCount = function () {
+            if (entity.presenceElus) {
+                var signaturesCountTotal = 0;
+                var awaited = entity.presenceElus.length;
+                entity.presenceElus.forEach(function (pe) {
+                    signaturesCountTotal += pe.signatures.length;
+                });
+                $scope.signaturesCount.totalMissing = awaited - signaturesCountTotal;
+            }
+        };
+
+        $scope.$watch('reunion.presenceElus', function () {
+            $scope.updateSignaturesCount();
+        });
+
         $scope.load = function (id) {
             ReunionCommissionThematique.get({id: id}, function(result) {
                 $scope.reunion = result;
