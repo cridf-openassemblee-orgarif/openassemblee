6f9ddd425fade03327716b9d92997ccde6544958
Mathieu Lorber <mathieu.lorber@gmail.com>
2019-04-15 11:52:48 +0200

    SAVEPOINT

--- src/main/java/openassemblee/service/OpenAssembleeMessageReceiver.java
+++ src/main/java/openassemblee/service/OpenAssembleeMessageReceiver.java
@@ -1,12 +1,11 @@
 package openassemblee.service;
 
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OpenAssembleeMessageReceiver {
 
-    @JmsListener(destination = "someQueue")
+//    @JmsListener(destination = "someQueue")
     public void processMessage(String content) {
         // ...
         System.out.println(content);
