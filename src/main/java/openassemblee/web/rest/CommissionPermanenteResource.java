08ab6d0f96d315b197fe0a7bca47c57d334d8954
Mathieu Lorber <mathieu.lorber@gmail.com>
2019-04-26 17:54:59 +0200

    Fix export PDF (quand connecté)

--- src/main/java/openassemblee/web/rest/CommissionPermanenteResource.java
+++ src/main/java/openassemblee/web/rest/CommissionPermanenteResource.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,6 +73,7 @@ public class CommissionPermanenteResource {
         }
     }
 
+    @Transactional(readOnly = true)
     @RequestMapping(value = "/commission-permanente/export-pdf",
         method = RequestMethod.GET,
         produces = MediaType.APPLICATION_JSON_VALUE)
