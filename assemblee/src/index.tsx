daaa458e827754ef413513671bf4b5a625483106
Mathieu Lorber <mathieu.lorber@gmail.com>
2020-01-14 19:22:07 +0100

    [hemicycle] Redimensionnement si agrandissement ou rétrécissement du menu adminlte

--- assemblee/src/index.tsx
+++ assemblee/src/index.tsx
@@ -9,6 +9,25 @@ window.addEventListener('resize', () =>
     injector().applicationEventBus.publish('window_resized_event')
 );
 
+// Ce code pour redimensionner l'assemblee à l'agrandissement/rétrécissement du menu admintle
+// setTimeout car sinon document.body est null à l'éxécution du code
+setTimeout(() => {
+    new MutationObserver(function(event) {
+        setTimeout(
+            () => {
+                injector().applicationEventBus.publish('window_resized_event');
+            },
+            // 500ms est le temps de l'animation d'agrandissement/rétrécissement
+            550
+        );
+    }).observe(document.body, {
+        attributes: true,
+        attributeFilter: ['class'],
+        childList: false,
+        characterData: false
+    });
+}, 0);
+
 global.loadAssemblee = () => {
     ReactDOM.render(<App />, document.getElementById('assemblee-app'));
 };
