245d58ddbf04b2025da394b026b0a5396b0cc667
Mathieu Lorber <mathieu.lorber@gmail.com>
2021-07-02 12:29:52 +0200

    Fix sécurité endpoint pour site public

--- src/main/java/openassemblee/config/SecurityConfiguration.java
+++ src/main/java/openassemblee/config/SecurityConfiguration.java
@@ -138,6 +138,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset_password/finish").permitAll()
             .antMatchers("/api/logs/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/api/audits/**").hasAuthority(AuthoritiesConstants.ADMIN)
+            .antMatchers("/api/publicdata/**").permitAll()
 
             // pour "publication"
             .antMatchers("/api/elus/**").permitAll()
