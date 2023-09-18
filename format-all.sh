7f6002468703bc4e940b26c6be9e7401e1c69b36
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-09-18 22:14:07 +0200

    Fix format all script

--- format-all.sh
+++ format-all.sh
@@ -1,1 +1,4 @@
-prettier --write "**/*.java"
+# https://github.com/jhipster/prettier-java
+# nvm use v18.12.1
+# npm install -g prettier prettier-plugin-java
+$HOME/.nvm/versions/node/v18.12.1/bin/prettier --write "**/*.java"
