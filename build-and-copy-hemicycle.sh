651938d085b03f85f2c50aa5837c12a83f851d27
Mathieu Lorber <mathieu.lorber@gmail.com>
2025-01-17 16:54:04 +0100

    Package script

--- build-and-copy-hemicycle.sh
+++
@@ -1,6 +0,0 @@
-trash src/main/webapp/hemicycle
-cd hemicycle
-yarn
-NODE_OPTIONS=--openssl-legacy-provider yarn build
-cd ..
-cp -R hemicycle/build/static/js src/main/webapp/hemicycle
---
+++ build-and-package.sh
@@ -0,0 +1,24 @@
+
+# HEMICYCLE
+trash src/main/webapp/hemicycle
+cd hemicycle
+yarn
+NODE_OPTIONS=--openssl-legacy-provider yarn build
+cd ..
+cp -R hemicycle/build/static/js src/main/webapp/hemicycle
+
+# FRONT
+npm install
+grunt build --force
+
+# BACK
+trash target
+mvn install -Pprod
+
+# PACKAGE
+trash .package
+mkdir .package
+mkdir .package/region
+mkdir .package/clevercloud
+cp target/openassemblee-1.0-SNAPSHOT.war.original .package/region/openassemblee.war
+cp target/openassemblee-1.0-SNAPSHOT.war .package/clevercloud/openassemblee.war
